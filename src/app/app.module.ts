fab8b4f4882a00c3e47e46f01f4d22de620e5222
tunglam99 <lamnthnue.edu.vn@gmail.com>
2019-11-19 11:05:58 +0700

    'ok'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import {NgModule} from '@angular/core';
+import {TimelinesComponent} from './timelines/timelines.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelinesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/date-util.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateUtilService {
+  getDiffToNow(arg0: any): any {
+    throw new Error('Method not implemented.');
+  }
+
+  constructor() { }
+}
